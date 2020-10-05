87efd939a3d8ab5a99bd11f6a108d59bfaea9a20
kai-blt <blaitong@gmail.com>
2020-10-05 14:49:28 -0700

    Got to do list to add items to the DOM

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,30 +1,41 @@
 import React, { useState } from 'react'; 
-import {render} from 'react-dom';
 
-const toDoItems = ['test'];
+
+//Create To Do Array
+const toDoItems = [];
 
 
 export default function ToDo(props) {
-
+    //Set up state hooks
     const [input, setInput] = useState('');
-    const [listItem, addListItem] = useState(toDoItems)  
+    const [listItem, addListItem] = useState(toDoItems); 
 
+    //Add to list helper function
+    const addToList = (input) => {
+       addListItem([...listItem, input]);     
+    }
 
-    const addLi = () => {
-       addListItem({input})
-       const li = <li>{toDoItems}</li>
-       render(li, document.querySelector('ul'));
+    const removeFromList = (event) => {
+        //Get the closest li from the event target
+        let liToRemove = event.target.closest('li');
+        console.log(listItem.indexOf(liToRemove.textContent));
+        addListItem([...listItem]);
     }
   
     return (
         <div className='container'>
-            <h2>To Do List:</h2>
+            <h2>My To Do List:</h2>
             <div>
-                <h3>Add item:</h3>
+                <h3>My To Do List:</h3>
                 <input type="text" value={input} onChange={event => setInput(event.target.value)}></input>
-                <button onClick={() => addLi()}>Add</button>
+                <button onClick={() => addToList(input)}>Add</button>
                 <ul>
-                
+                    {
+                        listItem.map((item, index) => {        
+                           return <li key={index}>{item} 
+                           <button onClick={() => removeFromList(index)}>x</button></li>
+                       })
+                    }
                 </ul>
             </div>
         </div>
